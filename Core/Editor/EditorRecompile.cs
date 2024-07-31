b5abab0e83c01bdd67345e9269b38e4d8856c8cf
Andrew Pickard <andrew.pickard@mimesis-studios.com>
2024-07-31 15:05:59 +0100

    Added - Editor utility to force recompile

---
+++ Core/Editor/EditorRecompile.cs
@@ -0,0 +1,10 @@
+﻿using UnityEditor;
+
+public class EditorRecompile : EditorWindow
+{
+    [MenuItem("Flux/Recompile", false, 1)]
+    public static void SetDefaultScene()
+    {
+        EditorUtility.RequestScriptReload();
+    }
+}
