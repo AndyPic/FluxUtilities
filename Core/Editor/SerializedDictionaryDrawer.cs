4224224e054f4162d962d716728b91251aecc6f0
Andrew Pickard <andrew.pickard@mimesis-studios.com>
2024-08-04 13:49:12 +0100

    Added - Serialized dictionary + drawer

---
+++ Core/Editor/SerializedDictionaryDrawer.cs
@@ -0,0 +1,88 @@
+﻿using Flux.Core;
+using UnityEditor;
+using UnityEngine;
+
+namespace FluxEditor
+{
+    [CustomPropertyDrawer(typeof(SerializedDictionary<,>))]
+    public class SerializedDictionaryDrawer : PropertyDrawer
+    {
+        private const string KeysName = "<Keys>k__BackingField";
+        private const string ValuesName = "<Values>k__BackingField";
+
+        private const float Spacing = 5f;
+        private const float KeyWidthPercentage = 0.4f;
+        private const float ValueWidthPercentage = 0.6f;
+        private const float DeleteWidth = 24f;
+        private const float HalfDeleteWidth = DeleteWidth / 2f;
+
+        private bool foldout = true;
+
+        public override float GetPropertyHeight(SerializedProperty property, GUIContent label)
+        {
+            SerializedProperty keysProperty = property.FindPropertyRelative(KeysName);
+
+            if (foldout)
+            {
+                return (EditorGUIUtility.singleLineHeight + Spacing) * (keysProperty.arraySize + 2);
+            }
+            else
+            {
+                return EditorGUIUtility.singleLineHeight;
+            }
+        }
+
+        public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+        {
+            EditorGUI.BeginProperty(position, label, property);
+
+            Rect foldoutRect = new(position.x, position.y, position.width, EditorGUIUtility.singleLineHeight);
+            foldout = EditorGUI.Foldout(foldoutRect, foldout, label);
+            if (foldout)
+            {
+                EditorGUI.indentLevel++;
+
+                SerializedProperty keysProperty = property.FindPropertyRelative(KeysName);
+                SerializedProperty valuesProperty = property.FindPropertyRelative(ValuesName);
+
+                float lineHeight = EditorGUIUtility.singleLineHeight;
+                Rect linePosition = new(position.x, position.y, position.width, lineHeight);
+                linePosition.y += lineHeight + Spacing;
+
+                float keyWidth = (position.width * KeyWidthPercentage) - Spacing - HalfDeleteWidth;
+                float valueWidth = (position.width * ValueWidthPercentage) - Spacing - HalfDeleteWidth;
+
+                for (int i = 0; i < keysProperty.arraySize; i++)
+                {
+                    SerializedProperty keyProperty = keysProperty.GetArrayElementAtIndex(i);
+                    SerializedProperty valueProperty = valuesProperty.GetArrayElementAtIndex(i);
+
+                    Rect keyPosition = new(linePosition.x, linePosition.y, keyWidth, lineHeight);
+                    Rect valuePosition = new(linePosition.x + keyWidth, linePosition.y, valueWidth, lineHeight);
+                    Rect deletePosition = new(linePosition.x + keyWidth + valueWidth, linePosition.y, DeleteWidth, lineHeight);
+
+                    EditorGUI.PropertyField(keyPosition, keyProperty, GUIContent.none);
+                    EditorGUI.PropertyField(valuePosition, valueProperty, GUIContent.none);
+
+                    if (GUI.Button(deletePosition, "-"))
+                    {
+                        keysProperty.DeleteArrayElementAtIndex(i);
+                        valuesProperty.DeleteArrayElementAtIndex(i);
+                    }
+
+                    linePosition.y += lineHeight + Spacing;
+                }
+
+                if (GUI.Button(new Rect(linePosition.x + keyWidth + valueWidth, linePosition.y, DeleteWidth, lineHeight), "+"))
+                {
+                    keysProperty.arraySize++;
+                    valuesProperty.arraySize++;
+                }
+
+                EditorGUI.indentLevel--;
+            }
+
+            EditorGUI.EndProperty();
+        }
+    }
+}
---
+++ Core/SerializedDictionary.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Flux.Core
+{
+    [Serializable]
+    public class SerializedDictionary<TKey, TValue> : IEnumerable
+    {
+        [field: SerializeField] public List<TKey> Keys { get; private set; } = new();
+        [field: SerializeField] public List<TValue> Values { get; private set; } = new();
+
+        public int Count { get { return Keys.Count; } }
+
+        public SerializedDictionary(params (TKey, TValue)[] inputs)
+        {
+            foreach ((TKey key, TValue value) in inputs)
+            {
+                Add(key, value);
+            }
+        }
+
+        public TValue this[TKey key]
+        {
+            get
+            {
+                if (Keys.Contains(key))
+                {
+                    return Values[Keys.IndexOf(key)];
+                }
+                else
+                {
+                    return default;
+                }
+            }
+
+            set
+            {
+                if (Keys.Contains(key))
+                {
+                    Values[Keys.IndexOf(key)] = value;
+                }
+                else
+                {
+                    Add_Internal(key, value);
+                }
+            }
+        }
+
+        public bool TryGetValue(TKey key, out TValue value)
+        {
+            int index = Keys.IndexOf(key);
+
+            if (index != -1)
+            {
+                value = Values[index];
+                return true;
+            }
+            else
+            {
+                value = default;
+                return false;
+            }
+        }
+
+        public void Add(TKey key, TValue value)
+        {
+            if (Keys.Contains(key))
+            {
+                return;
+            }
+
+            Add_Internal(key, value);
+        }
+
+        private void Add_Internal(TKey key, TValue value)
+        {
+            Keys.Add(key);
+            Values.Add(value);
+        }
+
+        public void Remove(TKey key)
+        {
+            if (!Keys.Contains(key))
+            {
+                return;
+            }
+
+            int index = Keys.IndexOf(key);
+            Keys.RemoveAt(index);
+            Values.RemoveAt(index);
+        }
+
+        public void Clear()
+        {
+            Keys.Clear();
+            Values.Clear();
+        }
+
+        public IEnumerator<(TKey, TValue)> GetEnumerator()
+        {
+            for (int i = 0; i < Keys.Count; i++)
+            {
+                yield return (Keys[i], Values[i]);
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
