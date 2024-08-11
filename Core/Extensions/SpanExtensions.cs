afa82be062f4d51deacc001a85e7766a4c85843c
Andrew Pickard <andrew.pickard@mimesis-studios.com>
2024-08-11 10:46:34 +0100

    Added - 'Count' extension method for spans

---
+++ Core/Extensions/SpanExtensions.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+public static class SpanExtensions
+{
+    public static bool Contains<T>(this Span<T> span, T target)
+    {
+        for (int i = 0; i < span.Length; i++)
+        {
+            if (span[i].Equals(target))
+                return true;
+        }
+        return false;
+    }
+}
