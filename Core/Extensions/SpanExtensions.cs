5a1c55d975db9046f12b3f05e28b87b2773e2a9e
Andrew Pickard <andrew.pickard@mimesis-studios.com>
2024-08-12 09:41:10 +0100

    Change - Added a ToString extention

--- Core/Extensions/SpanExtensions.cs
+++ Core/Extensions/SpanExtensions.cs
@@ -11,4 +11,16 @@ public static class SpanExtensions
         }
         return false;
     }
+
+    public static string ToStringExt<T>(this Span<T> span)
+    {
+        System.Text.StringBuilder sb = new();
+        sb.Append('[');
+        for (int i = 0; i < span.Length; i++)
+        {
+            sb.Append($"{span[i]}");
+        }
+        sb.Append(']');
+        return sb.ToString();
+    }
 }
