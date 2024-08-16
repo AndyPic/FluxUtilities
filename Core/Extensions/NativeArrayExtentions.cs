099a5347c3ae57fcc680a6f4a19314c6719cbeb9
Andrew Pickard <andrew.pickard@mimesis-studios.com>
2024-08-16 17:15:42 +0100

    Added - NativeArray extentions

---
+++ Core/Extensions/NativeArrayExtentions.cs
@@ -0,0 +1,24 @@
+﻿using System.Text;
+using Unity.Collections;
+
+public static class NativeArrayExtentions
+{
+    public static string ToStringExt<T>(NativeArray<T> array) where T : struct
+    {
+        var sb = new StringBuilder();
+
+        sb.Append("[");
+        for (int i = 0; i < array.Length; i++)
+        {
+            sb.Append(array[i].ToString());
+
+            if (i < array.Length - 1)
+            {
+                sb.Append(", ");
+            }
+        }
+        sb.Append("]");
+
+        return sb.ToString();
+    }
+}
