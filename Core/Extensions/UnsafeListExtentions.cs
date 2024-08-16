bb3660056b1c59fb42842189c180420ab44da94d
Andrew Pickard <andrew.pickard@mimesis-studios.com>
2024-08-16 17:15:54 +0100

    Added - UnsafeList ententions

---
+++ Core/Extensions/UnsafeListExtentions.cs
@@ -0,0 +1,25 @@
+﻿using System.Text;
+using Unity.Collections.LowLevel.Unsafe;
+using UnityEngine;
+
+public class UnsafeListExtentions : MonoBehaviour
+{
+    public static string ToStringExt<T>(UnsafeList<T> list) where T : unmanaged
+    {
+        var sb = new StringBuilder();
+
+        sb.Append("[");
+        for (int i = 0; i < list.Length; i++)
+        {
+            sb.Append(list[i].ToString());
+
+            if (i < list.Length - 1)
+            {
+                sb.Append(", ");
+            }
+        }
+        sb.Append("]");
+
+        return sb.ToString();
+    }
+}
