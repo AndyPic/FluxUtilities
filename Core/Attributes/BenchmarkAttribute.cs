3ee414d8a54f2f5c3c3772ee13dd0947e0a48b25
Andrew Pickard <andrew.pickard@mimesis-studios.com>
2024-08-16 17:13:14 +0100

    Change - Update docs + Now inherits from benchmark base class

--- Core/Attributes/BenchmarkAttribute.cs
+++ Core/Attributes/BenchmarkAttribute.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Drawing;
 
 namespace Flux.Core.Attributes
 {
@@ -11,11 +12,25 @@ namespace Flux.Core.Attributes
     /// the benchmark methods for.
     /// <br></br>
     /// The default itterations are: 1_000.
+    /// <br></br>
+    /// See also: <see cref="BenchmarkSetupAttribute"/> and <see cref="BenchmarkCleanupAttribute"/>
     /// </summary>
     [AttributeUsage(AttributeTargets.Method, AllowMultiple = false)]
-    public class BenchmarkAttribute : Attribute
+    public class BenchmarkAttribute : A_BenchmarkAttribute
     {
-        public string GroupName { get; set; } = "Default";
         public int[] Itterations { get; set; } = new int[] { 1_000 };
+        public Color Color { get; set; } = Color.White;
+
+        public BenchmarkAttribute(string groupName = "Default", params int[] itterations)
+        {
+            GroupName = string.IsNullOrEmpty(groupName) ? "Default" : groupName;
+            Itterations = itterations != null && itterations.Length > 0 ? itterations : new int[] { 1_000 };
+        }
+
+        // todo - Add a 'setup' function that can be run before the benchmark
+        // to setup any data for the benchmarking
+        // some enum options eg. run before each itteration, run once at start
+        // run before each method etc.
+        // and GroupName to specify which group it should run on
     }
 }
