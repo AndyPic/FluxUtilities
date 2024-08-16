539aa8c83d47fe7c43ca4bb5a58ca534451c9e5b
Andrew Pickard <andrew.pickard@mimesis-studios.com>
2024-08-16 17:15:34 +0100

    Added - Enum extentions

---
+++ Core/Extensions/EnumExtentions.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+public static class EnumExtentions
+{
+    public static string ToStringExt<T>(this T value) where T : Enum
+    {
+        string enumName = value.ToString();
+        string cleanedName = Regex.Replace(enumName, "(?<!^)([A-Z])", " $1");
+        return cleanedName;
+    }
+}
