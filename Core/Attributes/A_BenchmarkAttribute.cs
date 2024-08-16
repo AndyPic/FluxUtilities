b6cef9478099a58aad3d8e841956836506c332c6
Andrew Pickard <andrew.pickard@mimesis-studios.com>
2024-08-16 17:12:39 +0100

    Added - Base class for benchmark attributes

---
+++ Core/Attributes/A_BenchmarkAttribute.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace Flux.Core.Attributes
+{
+    public abstract class A_BenchmarkAttribute : Attribute
+    {
+        public string GroupName { get; set; } = "Default";
+    }
+}
