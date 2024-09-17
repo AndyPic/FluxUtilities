2c8aab37cd17a0e6fb6d7f6e11ec2e8f1126fe4f
Andrew Pickard <andrew.pickard@mimesis-studios.com>
2024-09-17 16:51:54 +0100

    Added - Save helpers

---
+++ Save/GameSaver.cs
@@ -0,0 +1,104 @@
+﻿using Flux.Encryption;
+using Newtonsoft.Json;
+using System;
+using System.IO;
+using System.Text;
+using UnityEngine;
+
+namespace Flux.Save
+{
+    public static class GameSaver
+    {
+        private const string Key = "cre$-SToSp_Qibr6kEs4p_L-p-gosIk!";
+        private const string IV = "@Ut0xln*#E&esu5I";
+        private const string SaveFileExt = ".sav";
+        private const string SavesFolderName = "saves";
+        private static readonly JsonSerializerSettings jsonSerializerSettings;
+        private static readonly string SavesFolderPath;
+
+        static GameSaver()
+        {
+            SavesFolderPath = Path.Combine(Application.persistentDataPath, SavesFolderName);
+
+            // Setup serializer settings
+            jsonSerializerSettings = new()
+            {
+                // Adds type names to the json, so can easily deserialize to correct types
+                TypeNameHandling = TypeNameHandling.All,
+
+                // Ignore recursive fields that would result in infinite loop
+                // (Vector3 contains one of these)
+                ReferenceLoopHandling = ReferenceLoopHandling.Ignore
+            };
+        }
+
+        public static void Save<T>(string saveName, T saveData)
+        {
+            string filePath = Path.Combine(SavesFolderPath, saveName + SaveFileExt);
+
+            // Serialise prefs to string
+            var serializedData = JsonConvert.SerializeObject(saveData, jsonSerializerSettings);
+
+            // Encrypt the json string
+            var encryptedData = Encryptor.AesEncrypt(serializedData, IV, Key);
+
+            // Encode to bytes
+            var binaryData = Encoding.UTF8.GetBytes(encryptedData);
+
+            // Create saves directory (if needed)
+            Directory.CreateDirectory(SavesFolderPath);
+
+            // Write the data to file
+            File.WriteAllBytes(filePath, binaryData);
+        }
+
+        public static bool Load<T>(string saveName, out T saveData)
+        {
+            string filePath = Path.Combine(Application.persistentDataPath, saveName + SaveFileExt);
+
+            // Guard if file doesn't exist
+            if (!File.Exists(filePath))
+            {
+                Debug.LogError($"File doesn't exist: {filePath}");
+                saveData = default;
+                return false;
+            }
+
+            try
+            {
+                // Read binary data from file
+                var binaryData = File.ReadAllBytes(filePath);
+
+                // Decode to string
+                var encodedData = Encoding.UTF8.GetString(binaryData);
+
+                // Decrypt to json
+                var serializedData = Encryptor.AesDecrypt(encodedData, IV, Key);
+
+                // Deserialize from json
+                saveData = JsonConvert.DeserializeObject<T>(serializedData, jsonSerializerSettings);
+
+                return true;
+            }
+            catch (Exception ex)
+            {
+                Debug.LogError($"Failed to load file: {filePath} \nError: {ex.Message}");
+                saveData = default;
+                return false;
+            }
+        }
+
+        public static string[] GetSaves()
+        {
+            string[] saveFiles = Directory.GetFiles(Application.persistentDataPath, "*" + SaveFileExt);
+
+            // Remove the path and extension, leaving only the file names
+            for (int i = 0; i < saveFiles.Length; i++)
+            {
+                saveFiles[i] = Path.GetFileNameWithoutExtension(saveFiles[i]);
+            }
+
+            return saveFiles;
+        }
+    }
+}
---
+++ Save/PreferenceSaver.cs
@@ -0,0 +1,166 @@
+﻿using Flux.Encryption;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Flux.Save
+{
+    public static class PreferenceSaver
+    {
+        private const int CypherOffset = 5;
+
+        public const string PrefsFolderName = "prefs";
+        public const string PrefsFileName = "preferences.sav";
+        public static readonly string PrefsFolderPath;
+        public static readonly string PrefsFilePath;
+
+        private static readonly Dictionary<string, object> preferences;
+        private static readonly JsonSerializerSettings jsonSerializerSettings;
+
+        private static CancellationTokenSource cancellationTokenSource;
+        private static bool hasUnsavedChanges = false;
+
+        static PreferenceSaver()
+        {
+            // Build file path
+            PrefsFolderPath = Path.Combine(Application.persistentDataPath, PrefsFolderName);
+            PrefsFilePath = Path.Combine(PrefsFolderPath, PrefsFileName);
+
+            // Setup serializer settings
+            jsonSerializerSettings = new()
+            {
+                // Adds type names to the json, so can easily deserialize to correct types
+                TypeNameHandling = TypeNameHandling.All,
+
+                // Ignore recursive fields that would result in infinite loop
+                // (eg. Vector3 contains one of these)
+                ReferenceLoopHandling = ReferenceLoopHandling.Ignore
+            };
+
+            // Read any cached preferences from file
+            preferences = ReadFromFile();
+
+            // Write any changes to file when quitting, if we have unsaved changes
+            // (Note: This won't work with force-quits, and is not called on certain
+            // platforms eg. mac, android etc.)
+            Application.quitting += WriteToFile;
+        }
+
+        public static void Save<T>(string key, T data, float writeDelay = 0)
+        {
+            // Add new data to preferences
+            preferences[key] = data;
+
+            // Update unsaved changes flag
+            hasUnsavedChanges = true;
+
+            if (writeDelay > 0)
+            {
+                DelayedWriteToFile(writeDelay);
+            }
+            else
+            {
+                WriteToFile();
+            }
+        }
+
+        public static bool Load<T>(string key, out T data)
+        {
+            if (preferences.TryGetValue(key, out var untypedData) && untypedData is T typedData)
+            {
+                data = typedData;
+                return true;
+            }
+            else
+            {
+                data = default;
+                return false;
+            }
+        }
+
+        private static void CancelCurrentTask()
+        {
+            // If there's an existing cancellationTokenSource, cancel it
+            if (cancellationTokenSource != null)
+            {
+                cancellationTokenSource.Cancel();
+                cancellationTokenSource = null;
+            }
+        }
+
+        private static async void DelayedWriteToFile(float delaySeconds)
+        {
+            // Cancel the current task, if we have one
+            CancelCurrentTask();
+
+            // Setup cancellation token for the new task
+            cancellationTokenSource = new CancellationTokenSource();
+            var cancellationToken = cancellationTokenSource.Token;
+
+            try
+            {
+                // Await the delay
+                await Task.Delay(TimeSpan.FromSeconds(delaySeconds), cancellationToken);
+            }
+            catch (TaskCanceledException)
+            {
+                // Task was canceled, do nothing
+                return;
+            }
+
+            // Finally, write to file
+            WriteToFile();
+        }
+
+        private static void WriteToFile()
+        {
+            // Guard if we have no changes to write to file.
+            if (!hasUnsavedChanges)
+                return;
+
+            // Cancel the current task, if we have one
+            CancelCurrentTask();
+
+            // Serialise prefs to string
+            var serializedData = JsonConvert.SerializeObject(preferences, Formatting.Indented, jsonSerializerSettings);
+
+            // Obfuscate serialized data before writing to file
+            var obfuscatedData = Encryptor.Obfuscate(serializedData, CypherOffset);
+
+            // Create directory (if needed)
+            Directory.CreateDirectory(PrefsFolderPath);
+
+            // Write the string data to file
+            File.WriteAllText(PrefsFilePath, obfuscatedData);
+            Debug.Log($"Wrote to : {PrefsFilePath}.");
+        }
+
+        private static Dictionary<string, object> ReadFromFile()
+        {
+            Dictionary<string, object> preferences;
+
+            // Build preferences from file, if it exists
+            if (File.Exists(PrefsFilePath))
+            {
+                // Read the serialized data from file
+                string serializedData = File.ReadAllText(PrefsFilePath);
+
+                // Deobfuscate serialized data after reading from file
+                var deobfuscatedData = Encryptor.Deobfuscate(serializedData, CypherOffset);
+
+                // Deserialize into preferences
+                preferences = JsonConvert.DeserializeObject<Dictionary<string, object>>(deobfuscatedData, jsonSerializerSettings);
+            }
+            else
+            {
+                preferences = new();
+            }
+
+            return preferences;
+        }
+    }
+}
