d44644005d2f695277efe3def1615a7908390808
Andrew Pickard <andrew.pickard@mimesis-studios.com>
2024-09-17 16:53:48 +0100

    Added - Panel graphic component base

---
+++ UserInterface/A_PanelGraphicComponent.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace Flux.UserInterface
+{
+    [RequireComponent(typeof(CanvasRenderer))]
+    public class A_PanelGraphicComponent : MaskableGraphic
+    {
+        [SerializeField] protected Panel panel;
+
+        protected override void Awake()
+        {
+            base.Awake();
+            panel ??= GetComponent<Panel>();
+        }
+    }
+}
