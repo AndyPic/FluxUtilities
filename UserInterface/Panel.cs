ea9390687c198b47bf37c38ecb485c7cbf51819a
Andrew Pickard <andrew.pickard@mimesis-studios.com>
2024-09-19 12:35:41 +0100

    Change - Added UUID and some utility functions

--- UserInterface/Panel.cs
+++ UserInterface/Panel.cs
@@ -14,7 +14,10 @@ namespace Flux.UserInterface
         [field: Tooltip("The collection of transitions this panel will use when enabling / disabling.")]
         [field: SerializeField] public TransitionCollection Transitions { get; private set; }
 
+        [field: Tooltip("A UNIVERSALLY UNIQUE IDENTIFIER for this panel.")]
+        [field: SerializeField] public uint UUID { get; private set; } = uint.MinValue;
 
+        [field: SerializeField] public Canvas Canvas { get; private set; }
 
         private TransitionHandler transitionHandler;
 
@@ -59,6 +62,41 @@ namespace Flux.UserInterface
         public void Disable()
         {
             transitionHandler.TransitionToOff(this);
+        }
+
+        private Vector2 GetScaledResolution()
+        {
+            return new Vector2(Screen.width / Canvas.scaleFactor, Screen.height / Canvas.scaleFactor);
+        }
+
+        public void SetPositionWithinScreen(Vector2 position, float screenPadding = 0)
+        {
+            var halfScaledResolution = GetScaledResolution() / 2;
+            var target = RectTransform;
+
+            // Update target position
+            target.anchoredPosition = GetPositionInRect(target, position, halfScaledResolution, screenPadding);
+        }
+
+        private Vector2 GetPositionInRect(RectTransform target, Vector2 position, Vector2 halfSize, float padding = 0)
+        {
+#if UNITY_EDITOR
+            // Validate the Anchor and Pivot
+            if (target.anchorMin.x != 0.5f || target.anchorMax.x != 0.5f || target.anchorMin.y != 0.5f || target.anchorMax.y != 0.5f)
+            {
+                Debug.LogError("Invalid Anchor or Pivot (Must be 0.5)");
+            }
+#endif
+
+            // Calculate min / max x and y
+            var maxX = halfSize.x - (target.rect.width * 0.5f) - padding;
+            var maxY = halfSize.y - (target.rect.height * 0.5f) - padding;
+
+            // Lock position to screen
+            position.x = Mathf.Clamp(position.x, -maxX, maxX);
+            position.y = Mathf.Clamp(position.y, -maxY, maxY);
+
+            return position;
         }
     }
 }
