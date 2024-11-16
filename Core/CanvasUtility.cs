0ac8193e65f8ee046b712d8284a998683d251ac0
Andrew Pickard <andypick1993@gmail.com>
2024-11-16 20:46:51 +0000

    Added - Canvas utilities

---
+++ Core/CanvasUtility.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+namespace Flux.Core
+{
+    public static class CanvasUtility
+    {
+        public static bool TryGetFirstCanvas(out Canvas canvas)
+        {
+#if UNITY_6000_0_OR_NEWER
+            canvas = Object.FindFirstObjectByType<Canvas>();
+#else
+            canvas = Object.FindObjectOfType<Canvas>();
+        
+#endif
+            return canvas == null;
+        }
+    }
+}
