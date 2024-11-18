aa44972eb1650b04edbd15e3bfb7e9ab4fe1767a
Andrew Pickard <andypick1993@gmail.com>
2024-11-18 11:21:05 +0000

    Added - New attribute for an editor button with confirmation modal

---
+++ Core/Attributes/ButtonWithConfirmationAttribute.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Flux.Core.Attributes
+{
+    [AttributeUsage(AttributeTargets.Method, AllowMultiple = false)]
+    public class ButtonWithConfirmationAttribute : Attribute
+    {
+        public string message;
+        public string label;
+
+        public ButtonWithConfirmationAttribute(string message = "Please confirm the action", string label = "")
+        {
+            this.message = message;
+            this.label = label;
+        }
+    }
+}
